db68b68a3803adc9670c5e735b031816809b6008
David Himmelstrup <lemmih@gmail.com>
2025-02-28 11:20:46 +0100

    testing: basic e2e testing (#12)

--- flake.nix
+++ flake.nix
@@ -14,6 +14,10 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    e2e = {
+      url = "path:./e2e";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
@@ -23,6 +27,7 @@
     pot-cli,
     pot-web,
     rust-overlay,
+    e2e,
   }:
     utils.lib.eachDefaultSystem (
       system: let
@@ -30,17 +35,54 @@
           inherit system;
           overlays = [(import rust-overlay)];
         };
+
+        e2e-test = pkgs.writeShellScriptBin "e2e-test" ''
+          # Start the web service
+          ${pot-web.apps.${system}.default.program} &
+          WEB_PID=$!
+
+          ${pkgs.geckodriver}/bin/geckodriver --port 4444 &
+          GECKO_PID=$!
+
+          # Run the tests
+          ${self.packages.${system}.e2e}/bin/e2e
+          TEST_EXIT=$?
+
+          # Clean up
+          kill $WEB_PID
+          kill $GECKO_PID
+          exit $TEST_EXIT
+        '';
       in {
         packages = {
           pot-cli = pot-cli.packages.${system}.default;
           pot-web = pot-web.packages.${system}.default;
+          e2e = e2e.packages.${system}.default;
           default = self.packages.${system}.pot-cli;
         };
         devShell = with pkgs;
           mkShell {
-            buildInputs = [cargo rustc rustfmt pre-commit rustPackages.clippy lld wasm-pack wasm-bindgen-cli tailwindcss binaryen];
+            buildInputs = [
+              cargo
+              rustc
+              rustfmt
+              pre-commit
+              rustPackages.clippy
+              lld
+              wasm-pack
+              wasm-bindgen-cli
+              tailwindcss
+              binaryen
+              geckodriver
+            ];
             RUST_SRC_PATH = rustPlatform.rustLibSrc;
           };
+        apps = {
+          e2e-test = {
+            type = "app";
+            program = "${e2e-test}/bin/e2e-test";
+          };
+        };
       }
     );
 }
